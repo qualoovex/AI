0034ed320b8cbbf665a6c2a0b2de44b22294ecb9
Granger <julian.granger@icloud.com>
2024-03-14 14:02:08 -0400

    printing names index - stable - preGPT

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -3,3 +3,9 @@
 #program printing names index
 #resource https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
+names = ["Corey", "Chris", "Travis", "Dave"]
+
+index = 0
+for name in names:
+    print(index, name)
+    index += 1
